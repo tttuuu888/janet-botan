84f5e85dfef0a03edc1b92b0457a63577f5bd03f
Seungki Kim <tttuuu888@gmail.com>
2024-02-27 00:52:08 +0900

    Add cfun_version_string and test

--- src/versioning.h
+++ src/versioning.h
@@ -21,12 +21,22 @@ static Janet cfun_ffi_supports_api(int32_t argc, Janet *argv) {
     return janet_wrap_number((double)ret);
 }
 
+static Janet cfun_version_string(int32_t argc, Janet *argv) {
+    (void)argc;
+    (void)argv;
+    const char *version = botan_version_string();
+    return janet_wrap_string((const uint8_t *)version);
+}
+
 static JanetReg default_cfuns[] = {
     {"ffi-api-version", cfun_ffi_api_version, "(ffi-api-version)\n\n"
       "Return the version of the currently supported FFI API."
     },
     {"ffi-supports-api", cfun_ffi_supports_api, "(ffi-supports-api)\n\n"
      "Returns 0 if the FFI version specified is supported by this library."
+    },
+    {"version-string", cfun_version_string, "(version-string)\n\n"
+     "Returns a free-form string describing the version."
     },
     {NULL, NULL, NULL}
 };
--- test/test_versioning.janet
+++ test/test_versioning.janet
@@ -3,3 +3,5 @@
 (assert (= (ffi-api-version) 20231009))
 
 (assert (= (ffi-supports-api (ffi-api-version)) 0))
+
+(assert (= (string/slice (version-string) 0 11) "Botan 3.3.0"))
