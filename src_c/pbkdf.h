7794ab37cee345cfddf80f28407a063978083cd4
Seungki Kim <tttuuu888@gmail.com>
2024-03-14 00:27:46 +0900

    Update pbkdf, add pbkdf-timed and tests

--- src_c/pbkdf.h
+++ src_c/pbkdf.h
@@ -48,12 +48,76 @@ static Janet cfun_pbkdf(int32_t argc, Janet *argv) {
                         salt, salt_len);
     JANET_BOTAN_ASSERT(ret);
 
+    out->count = out_len;
+    Janet output[3] = {
+        janet_wrap_string(janet_string(salt, salt_len)),
+        janet_wrap_number((double)iter),
+        janet_wrap_string(janet_string(out->data, out->count))
+    };
+
     if (argc != 5) {
         janet_sfree(salt);
     }
 
+    return janet_wrap_tuple(janet_tuple_n(output, 3));
+}
+
+static Janet cfun_pbkdf_timed(int32_t argc, Janet *argv) {
+    janet_arity(argc, 3, 5);
+    const char *algo = janet_getcstring(argv, 0);
+    JanetByteView pw = janet_getbytes(argv, 1);
+    size_t out_len = janet_getsize(argv, 2);
+    JanetBuffer *out = janet_buffer(out_len);
+
+    size_t ms_to_run = 300;
+    size_t iter;
+    uint8_t *salt;
+    size_t salt_len;
+    int ret;
+
+    if (argc >= 4) {
+        ms_to_run = janet_getsize(argv, 3);
+    }
+
+    if (argc == 5) {
+        JanetByteView salt_data = janet_getbytes(argv, 4);
+        salt = (uint8_t *)salt_data.bytes;
+        salt_len = salt_data.len;
+    } else {
+        botan_rng_t rng;
+        salt_len = 12;
+        salt = janet_smalloc(salt_len);
+
+        ret = botan_rng_init(&rng, "system");
+        JANET_BOTAN_ASSERT(ret);
+
+        ret = botan_rng_get(rng, salt, salt_len);
+        JANET_BOTAN_ASSERT(ret);
+
+        ret = botan_rng_destroy(rng);
+        JANET_BOTAN_ASSERT(ret);
+    }
+
+    ret = botan_pwdhash_timed(algo, ms_to_run,
+                              &iter, 0, 0,
+                              out->data, out_len,
+                              (char *)pw.bytes, pw.len,
+                              salt, salt_len);
+    JANET_BOTAN_ASSERT(ret);
+
     out->count = out_len;
-    return janet_wrap_string(janet_string(out->data, out->count));
+
+    Janet output[3] = {
+        janet_wrap_string(janet_string(salt, salt_len)),
+        janet_wrap_number((double)iter),
+        janet_wrap_string(janet_string(out->data, out->count))
+    };
+
+    if (argc != 5) {
+        janet_sfree(salt);
+    }
+
+    return janet_wrap_tuple(janet_tuple_n(output, 3));
 }
 
 static JanetReg pbkdf_cfuns[] = {
@@ -65,6 +129,14 @@ static JanetReg pbkdf_cfuns[] = {
      "randomly chosen. Returns `out_len` bytes of output (or potentially "
      "less depending on the algorithm and the size of the request). "
      "Returns tuple of salt, iterations, and psk"
+    },
+    {"pbkdf-timed", cfun_pbkdf_timed,
+     "(pbkdf-timed algo passphrase out_len &opt ms-to-run salt)\n\n"
+     "Derive a key from a `passphrase` for a number of "
+     "Runs for as many iterations as needed to consumed `ms-to-run` "
+     "milliseconds on whatever we’re running on. Returns tuple of salt, "
+     "iterations, and psk. Default value of `ms_to_run` is 300 and `salt` "
+     "is 12 bytes of random values."
     },
     {NULL, NULL, NULL}
 };
--- test/test_pbkdf.janet
+++ test/test_pbkdf.janet
@@ -3,13 +3,17 @@
 
 (start-suite "pbkdf")
 
-(assert (= (hex-encode (pbkdf "PBKDF2(SHA-256)" "abcd" 32 1001
-                              (hex-decode "102030405060708090A0B0C0D0E0F000")))
-           (hex-encode (pbkdf "PBKDF2(SHA-256)" "abcd" 32 1001
-                              (hex-decode "102030405060708090A0B0C0D0E0F000")))
-           "DECF9EF197B87ABBDB6CBA9E81A7BCB8AC36BB2BFA3B93746C8042227A27CFEA"))
+(let [salt (hex-decode "102030405060708090A0B0C0D0E0F000")
+      expected-output (hex-decode "DECF9EF197B87ABBDB6CBA9E81A7BCB8AC36BB2BFA3B93746C8042227A27CFEA")]
+  (assert (= (pbkdf "PBKDF2(SHA-256)" "abcd" 32 1001 salt)
+             (pbkdf "PBKDF2(SHA-256)" "abcd" 32 1001 salt)
+             [salt 1001 expected-output])))
 
 (assert (not= (pbkdf "PBKDF2(SHA-256)" "abcd" 32)
               (pbkdf "PBKDF2(SHA-256)" "abcd" 32)))
+
+(let [[salt iter psk] (pbkdf-timed "PBKDF2(SHA-256)" "abcd" 32)]
+  (assert (= [salt iter psk]
+             (pbkdf "PBKDF2(SHA-256)" "abcd" 32 iter salt))))
 
 (end-suite)
